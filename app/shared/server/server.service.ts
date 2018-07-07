23ba3722b3dbb992214a51763e072d1baeee6147
David Lively <lively@iamrobots.com>
2018-07-07 10:59:54 -0700

    Postion right for create account

---
+++ app/shared/server/server.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, OnInit } from '@angular/core';
+
+import { Task, Duration } from '~/shared/tasks/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServerService implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  
+}
