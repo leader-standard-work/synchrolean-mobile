b4af0b244cde72ccd67220e760b072e59716257e
David Lively <lively@iamrobots.com>
2018-07-20 15:57:37 -0700

    Bottom tabbar navigates to tasks and teams

--- app/pages/teams/team-list/team-list.component.ts
+++ app/pages/teams/team-list/team-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Team } from '~/shared/teams/team';
 import { Observable } from 'rxjs';
 import { ServerService } from '~/shared/server/server.service';
+import { RouterExtensions } from 'nativescript-angular/router';
 
 @Component({
   selector: 'team-list',
@@ -12,15 +13,29 @@ import { ServerService } from '~/shared/server/server.service';
 export class TeamListComponent implements OnInit {
   public teams$: Array<Team>;
 
-  constructor(private server:ServerService){
-
-  }
+  constructor(
+    private server: ServerService,
+    private routerExtension: RouterExtensions
+  ) {}
 
   ngOnInit(): void {
     //disclaimer, this is testing with dummy data
     this.server.getTeams().subscribe(
-      teams => {this.teams$ = JSON.parse(JSON.stringify(teams))}, 
-      err => {console.log(err);} );
-    }
+      teams => {
+        this.teams$ = JSON.parse(JSON.stringify(teams));
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 
+  tasksTapped() {
+    this.routerExtension.navigate(['/task-list'], {
+      clearHistory: true,
+      transition: {
+        name: 'fade'
+      }
+    });
+  }
 }
