ca8e09c4425b1ab6ee5e261b1f0dbaae3b560b46
David Lively <lively@iamrobots.com>
2018-07-30 15:53:41 -0700

    Hanging on login before going to team page

--- app/pages/teams/team-list/team-list.component.ts
+++ app/pages/teams/team-list/team-list.component.ts
@@ -4,7 +4,7 @@ import { ServerService } from '~/shared/services/server.service';
 import { RouterExtensions } from 'nativescript-angular/router';
 import { ObservableArray } from 'data/observable-array';
 import { AccountService } from '~/shared/services/account.service';
-var appSettings = require("application-settings");
+var appSettings = require('application-settings');
 @Component({
   selector: 'team-list',
   moduleId: module.id,
@@ -13,7 +13,6 @@ var appSettings = require("application-settings");
 })
 export class TeamListComponent implements OnInit {
   public teams$: ObservableArray<Team>;
-  public loginButtonText: string;
 
   constructor(
     private serverService: ServerService,
@@ -23,13 +22,12 @@ export class TeamListComponent implements OnInit {
 
   ngOnInit() {
     this.teams$ = new ObservableArray<Team>();
-    this.loginButtonText = 'Sign in';
-    if(appSettings.getString('email') && !this.serverService.isLoggedIn()){
-      this.login();
-      return;
-    }
+    // this.loginButtonText = 'Sign in';
+    // if(appSettings.getString('email') && !this.serverService.isLoggedIn()){
+    //   this.login();
+    //   return;
+    // }
     if (this.serverService.isLoggedIn()) {
-      this.loginButtonText = 'Logout';
       this.serverService.getTeams().subscribe(
         teams => {
           teams.forEach(team => this.teams$.push(team));
@@ -39,38 +37,43 @@ export class TeamListComponent implements OnInit {
           console.error('could not get teams', error);
         }
       );
+    } else {
+      this.routerExtensions.navigate(['/login'], {
+        transition: {
+          name: 'slideTop'
+        }
+      });
     }
-
   }
 
   isLoggedIn(): boolean {
     return this.serverService.isLoggedIn();
   }
 
-  login(){
-    let userName = appSettings.getString("email");
-    let password = "Rookso06";
-    let serverUrl = appSettings.getString("serverUrl");
-   
-    this.serverService.login(serverUrl, userName, password).subscribe(
-      res=>{
-        this.serverService.autoLogin();
-        this.loginButtonText = 'Logout';
-        this.serverService.getTeams().subscribe(
-          teams => {
-            teams.forEach(team => this.teams$.push(team));
-            console.log(teams);
-          },
-          error => {
-            console.error('could not get teams', error);
-          }
-        );
-      }
-      ,err=>{
-        console.log(err);
-      });
+  // login(){
+  //   let userName = appSettings.getString("email");
+  //   let password = "Rookso06";
+  //   let serverUrl = appSettings.getString("serverUrl");
 
-  }
+  //   this.serverService.login(serverUrl, userName, password).subscribe(
+  //     res=>{
+  //       this.serverService.autoLogin();
+  //       this.loginButtonText = 'Logout';
+  //       this.serverService.getTeams().subscribe(
+  //         teams => {
+  //           teams.forEach(team => this.teams$.push(team));
+  //           console.log(teams);
+  //         },
+  //         error => {
+  //           console.error('could not get teams', error);
+  //         }
+  //       );
+  //     }
+  //     ,err=>{
+  //       console.log(err);
+  //     });
+
+  // }
 
   onTap(id: number) {
     this.routerExtensions.navigate(['/Members', id], {
@@ -80,17 +83,11 @@ export class TeamListComponent implements OnInit {
     });
   }
 
-  loginTapped() {
+  logoutTapped() {
     if (this.serverService.isLoggedIn()) {
       this.serverService.logout();
-      this.loginButtonText = 'Sign In';
+      // this.loginButtonText = 'Sign In';
       this.teams$ = new ObservableArray<Team>();
-      this.routerExtensions.navigate(['/teams'], {
-        transition: {
-          name: 'slideTop'
-        }
-      });
-    } else {
       this.routerExtensions.navigate(['/login'], {
         transition: {
           name: 'slideTop'
--- app/shared/services/account.service.ts
+++ app/shared/services/account.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Account } from '~/shared/models/account';
 import { ServerService } from '~/shared/services/server.service';
-var appSettings = require("application-settings");
+var appSettings = require('application-settings');
 
 export enum State {
   LoggedIn,
@@ -16,15 +16,40 @@ export class AccountService {
   private _account: Account;
   private _state: State = State.LoggedOut;
 
-  constructor() {}
+  constructor() {
+    if (
+      appSettings.hasKey('email') &&
+      appSettings.hasKey('firstname') &&
+      appSettings.hasKey('lastname') &&
+      appSettings.hasKey('ownerId') &&
+      appSettings.hasKey('serverUrl')
+    ) {
+      let email = appSettings.getString('email');
+      let firstname = appSettings.getsetString('firstname');
+      let lastname = appSettings.getsetString('lastname');
+      let ownerId = appSettings.getsetNumber('ownerId');
+      let serverUrl = appSettings.getsetString('serverUrl');
+
+      let account = new Account({
+        ownerId: ownerId,
+        firstName: firstname,
+        lastName: lastname,
+        email: email,
+        isDeleted: false
+      });
+      this._account = account;
+      this._account.serverUrl = serverUrl;
+      this._state = State.LoggedIn;
+    }
+  }
 
   get state(): State {
     return this._state;
   }
 
-  set state(state:State) {
-    this._state = state;
-  }
+  // set state(state: State) {
+  //   this._state = state;
+  // }
 
   get serverUrl(): string {
     if (this._account === null) {
@@ -43,14 +68,12 @@ export class AccountService {
     this._account = account;
     this._state = State.LoggedIn;
 
-    appSettings.setString("email", account.email);
-    appSettings.setString("firstname", account.firstname);
-    appSettings.setString("lastname", account.lastname);
-    appSettings.setNumber("ownerId", account.ownerId);
-    appSettings.setString("serverUrl", account.serverUrl);
+    appSettings.setString('email', account.email);
+    appSettings.setString('firstname', account.firstname);
+    appSettings.setString('lastname', account.lastname);
+    appSettings.setNumber('ownerId', account.ownerId);
+    appSettings.setString('serverUrl', account.serverUrl);
     //appSettings.setString("token", account.token);
-
-    console.log(appSettings.getString("firstname"));
   }
 
   get account(): Account {
