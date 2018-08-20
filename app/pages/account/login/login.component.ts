ff4da781a6550d22848794093831e1a397fce0ee
edward <edward@pdx.edu>
2018-08-19 21:46:08 -0700

    localhost

--- app/pages/account/login/login.component.ts
+++ app/pages/account/login/login.component.ts
@@ -14,7 +14,7 @@ import { AccountService } from '~/shared/services/account.service';
 })
 export class LoginComponent implements OnInit {
   public accountFormGroup: FormGroup;
-  public tempUrl: string = 'http://localhost';
+  public tempUrl: string = 'http://localhost:55542';
 
   constructor(
     private authService: AuthenticationService,
